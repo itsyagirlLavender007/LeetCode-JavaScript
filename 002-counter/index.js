d1f33799026afb0fb7cf004074566e37fd26218a
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-03 11:11:10 +0530

    index.js

--- 002-counter/index.js
+++ 002-counter/index.js
@@ -1,4 +1,4 @@
-// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+/* Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc). */
 
  /**
  * @param {number} n
@@ -16,9 +16,12 @@ counter();
 counter();
 counter();
 
-//What I understood:-
+/*
+What I understood:-
 
-//1. Make a function that first gives the passed value so in this case 10 and in the next function call it increases it by one i.e 11 and so on.
-//2. Simply use the [n ++] operator or use [n + 1].
-//3. You can also use a count variable to first store the value of n and then return the increment of the count variable which will be copied in n. 
-//4. Create a counter variable to call the createCounter(10) function or just use console.log(createCounter(10)).
+1. Make a function that first gives the passed value so in this case 10 and in the next function call it increases it by one i.e 11 and so on.
+2. Simply use the [n ++] operator or use [n + 1].
+3. You can also use a count variable to first store the value of n and then return the increment of the count variable which will be copied in n. 
+4. Create a counter variable to call the createCounter(10) function or just use console.log(createCounter(10)).
+
+*/
