a237022228d31e6d3b4d7efa2c948d9797105d0c
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-10 20:15:02 +0530

    index.js

--- 015-interval-cancellation/index.js
+++ 015-interval-cancellation/index.js
@@ -7,7 +7,7 @@ After a delay of cancelTimeMs, the returned cancel function cancelFn will be inv
 setTimeout(cancelFn, cancelTimeMs)
 The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
 
-*/
+*/ 
 
  
 /**
