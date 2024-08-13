5e8c3ff34d8ecd8348c08f1b551c50decf17e30c
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-14 00:16:07 +0530

    index.js

--- 019-execute-asynchronous-functions-in-parallel/index.js
+++ 019-execute-asynchronous-functions-in-parallel/index.js
@@ -37,4 +37,14 @@ var promiseAll = function(functions) {
 
 How to solve:-
 
+1. We have to return a new promise so we will use 'return new Promise()' directly. We have to make sure that the given array of functions are resolved at the same time. If any of the promise gets rejected then we have to return that particular promise that got rejected.
+
+2. We will create a variable 'promiseArray' that will store the results of the resolved functions and it's length will be similar to the length of the array of functions. We will assign that length using 'Array(functions.length)'.
+
+3. We will create under varible 'reducingLength' that will hold the length of the array of functions.
+
+4. We will use 'for loop' to iterate through the array of functions and after doing that we will use '.then()' with each of the function at that position which is considered as 'i' at this point, to get the result of each function at that position and store it in the 'promiseArray' array. With each execution of then we will also check the length using 'reducingLength--' to see how many functions are remaining. This way if we ever get any error then it will get caught/rejected easily.
+
+5. If the length of 'reducingLength' becomes 0 then we can say that all the functions have been iterated over and then we resolve their results together by using 'resolve(promiseArray)'.
+
 */
