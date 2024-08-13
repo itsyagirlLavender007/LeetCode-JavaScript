67821ab4839d34811086d873f7f4ae8b7db279d9
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-13 13:03:35 +0530

    Add files via upload

---
+++ 020-is-object-empty/index.js
@@ -0,0 +1,29 @@
+/*
+
+Given an object or an array, return if it is empty.
+
+An empty object contains no key-value pairs.
+An empty array contains no elements.
+You may assume the object or array is the output of JSON.parse.
+
+*/
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(Array.isArray(obj)){
+        return obj.length === 0
+    }
+    else{
+        let arr = Object.keys(obj)
+        return arr.length === 0
+    }
+};
+
+/*
+
+How to solve:-
+
+*/
