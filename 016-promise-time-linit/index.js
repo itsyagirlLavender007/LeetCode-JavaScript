c06642534a93ce90de8877f726d3fe6106e3b80b
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-13 22:54:51 +0530

    index.js

--- 016-promise-time-linit/index.js
+++ 016-promise-time-linit/index.js
@@ -31,4 +31,63 @@ var timeLimit = function(fn, t) {
 
 How to solve:-
 
+1. It's an async function so we have to use promise to handle it. 
+
+2. We use setTimeout to check the function against the given time limit. If the function resolves within time t milliseconds then we resolve it or we reject it. We will reject the function directly in the setTimeout.
+
+3. In order to resolve the function we will use 'fn(...args).then(resolve).catch(reject)'. Through this we handle both the cases that is resolve and reject. To resolve the function we use '.then(resolve)' and to reject we use '.catch(reject)' right after it.
+
+Note 1: Example of an async function using Promises
+
+const isAllowed = new Promise((resolve, reject)=>{
+    const age = prompt("What is your age? \n");
+    if(age >= 18){
+        resolve();
+    }
+    else{
+        reject();
+    }
+})
+
+isAllowed.then(() => console.log("You are allowed")).catch(()=> console.log("You are not allowed"));
+
+[
+Output: What is your age?
+18
+You are allowed
+]
+
+Note 2: Another example of async function using await
+
+function isAllowed() {
+    return new Promise((resolve, reject)=>{
+    const age = prompt("What is your age? \n");
+    if(age >= 18){
+        resolve("You are allowed");
+    }
+    else{
+        reject("You are not allowed");
+    }
+})
+}
+
+async function myFunction(){
+    try{
+    const value = await isAllowed()
+    console.log(value);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+myFunction()
+
+[
+Output: What is your age?
+18
+You are allowed
+]
+
+
 */
