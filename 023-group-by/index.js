f0bee1e5c407c53e6efc5e8450d3c65f96de857a
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-14 19:05:54 +0530

    index.js

--- 023-group-by/index.js
+++ 023-group-by/index.js
@@ -32,6 +32,32 @@ Array.prototype.groupBy = function(fn) {
 
 How to solve:-
 
+Note 1: Example of using lodash's _.groupBy function
 
+const _ = require('lodash');
+
+const items = [
+    { name: 'Item1', category: 'A' },
+    { name: 'Item2', category: 'B' },
+    { name: 'Item3', category: 'A' },
+    { name: 'Item4', category: 'C' },
+    { name: 'Item5', category: 'B' }
+];
+
+const groupedByCategory = _.groupBy(items, 'category');
+
+console.log(groupedByCategory);
+
+1. We use reduce() method here. reduce() is used to accumulate results into a single object (grouped). The reduce() method iterates through each 'item' in the array and performs the grouping logic.
+
+2. We create a variable 'key' that stores the result of function 'fn'. We apply function 'fn' on each 'item' to determine the group key. 
+
+3. We use 'if (!grouped[key])' to check if the key already exists in the grouped object. If not we intialize an empty array for that key.
+
+4. We use 'grouped[key].push(item)' to add the item to the appropriate array in the grouped object based on its key.
+
+5. 'return grouped' returns the updated grouped object after processing each item.
+
+6. The initial value for the reduce function is an empty object '{}'. It starts off as an empty object, and as items are processed, it gets filled with groups.
 
 */
