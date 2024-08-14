72bd0364797774cb00755c364b4f10224b776873
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-14 15:20:39 +0530

    index.js

--- 021-chunk-array/index.js
+++ 021-chunk-array/index.js
@@ -32,4 +32,30 @@ var chunk = function(arr, size) {
 
 How to solve:-
 
+1. Consider an array, arr = [1,2,3,4,5,6] and if the chunk size is 2 then the array will become arr= [[1,2],[3,4],[5,6]] this is a chunked array. If the size was 2 and we have only 5 array elements then the last subarray would only contain 1 element.
+
+2. We create an empty array 'result' that will hold the chunked array.
+
+3. Using a 'for loop' we will iterate through the original array and we will create a variable 'chunk' that will apply the slice() method on the given array 'arr'. 
+
+4. The slice(start,end) method takes two parameters that is start and end so we will use this to cut our array according to the chunk size. 
+
+5. 'arr.slice(i, i + size)', in this 'i' will be the array at 'ith' index and then 'i + size' will add the index of our array at 'ith' position with the 'size' so that we can cut the array from that 'ith' position to the 'size' that is given.
+
+Note 1: Example 
+
+let chunk = arr.slice(i, i + size);
+console.log(chunk);
+
+Output:
+
+[ 1, 2 ]
+[ 3, 4 ]
+[ 5 ]
+
+6. After this we will push the 'chunk' into the array 'result' with every iteration.
+
+7. We will return the 'result' outside of the 'for loop'.
+
+
 */
