84859f7f9be7baadfb43ec4cc4e54de754da1595
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-15 01:43:09 +0530

    index.js

--- 027-compact-object/index.js
+++ 027-compact-object/index.js
@@ -41,4 +41,16 @@ var compactObject = function(obj) {
 
 How to solve:-
 
+1. First we check if the input 'obj' is null and if the given 'obj' is of type object since the falsy value only applies to object. 
+
+2. If the input is an array then we use 'obj.filter(Boolean)' which filters out falsy values (false, 0, "", null, undefined, and NaN). The '.map(compactObject)' recursively applies 'compactObject' to each item in the array, ensuring nested arrays are also compacted.
+
+3. If the input is an object then first we create a new object 'compacted' that will store the truthy values after filtering the input of falsy values.
+
+4. We use 'for in' to iterate over the object with the help of its key. We create another variable 'value' that now holds the compacted version of obj[key], which could be a cleaned-up object, array, or a primitive value.
+
+5. After compacting we check if the 'value' is truthy or not. If it is then we add it to 'compacted' object with corresponding 'key'. 
+
+6. After this we return the 'compacted' object using 'return compacted'.
+
 */
