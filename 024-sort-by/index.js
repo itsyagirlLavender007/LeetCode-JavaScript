25f59c45382182cfe49b7042b3bce08b4b6c42e5
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-14 23:44:03 +0530

    index.js

--- 024-sort-by/index.js
+++ 024-sort-by/index.js
@@ -19,4 +19,22 @@ var sortBy = function(arr, fn) {
 
 How to solve:-
 
+1. Since we have to sort the functions based on their results in ascending order which are going to be numbers so we a sort() method.
+
+2. There is another sort() method sort(compareFn). 
+
+Note 1: 
+
+sort(compareFn) = A function that determines the order of the elements. The function is called with the following arguments:
+
+a - The first element for comparison. Will never be undefined.
+
+b - The second element for comparison. Will never be undefined.
+
+(a, b) => a - b sorts numbers in ascending order
+
+3. The comparison function '(a, b) => fn(a) - fn(b)' determines the order of a and b based on the numeric values returned by fn.
+
+4. If 'fn(a) - fn(b)' is negative, a comes before b. If positive, b comes before a. If zero, their order remains unchanged.
+
 */
